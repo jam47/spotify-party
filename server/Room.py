f318e2124ce8fc8d173846d748da458ab89b9003
Alan Chodyniecki <amc40@st-andrews.ac.uk>
2019-03-02 22:03:53 +0000

    Fixed bugs in Room.py class

--- server/Room.py
+++ server/Room.py
@@ -1,40 +1,43 @@
 class PartyRoom:
     DEFAULT_VOTES = 0
+
+
     def __init__(self):
-        self.songList = []
-        self.currentlyPlayingSong = NOne
+        self.currentlyPlayingSong = None
         self.isActive = True
+        self.songList = []
 
     def addSong(self, jsonSong):
         votedSong = {
-            "song":jsonSong,
-            "votes":0,
-            "played":false
+            "song": jsonSong,
+            "votes": 0,
+            "played": False
         }
 
         i = 0
-        while (songList[i]["votes"] > 0):
-            i++
-        songList.insert(i + 1, votedSong)
+        while self.songList[i]["votes"] > 0:
+            i += 1
+        self.songList.insert(i + 1, votedSong)
 
     def modifySongVotes(self, uri, voteModification):
         toModify = None
-        for song in songList:
-            if (song["song"]["uri"] == uri):
+        for song in self.songList:
+            if song["song"]["uri"] == uri:
                 toModify = song
         if (toModify != None):
-            songList.remove(toModify)
+            self.songList.remove(toModify)
             toModify["votes"] += voteModification
             newVotes = toModify["votes"]
             i = 0
-            while (songList[i]["votes"] > newVotes):
-                i++
-            songList.insert(i + 1, toModify)
+            while self.songList[i]["votes"] > newVotes:
+                i += 1
+            self.songList.insert(i + 1, toModify)
 
-    def getMostUpvotedNotPlayed():
-        for song in songList:
-            if (!song["played"])
+    def getMostUpvotedNotPlayedToPlay(self):
+        for song in self.songList:
+            if (not song["played"]):
+                song["Played"] = True
                 return song["song"]
 
-    def isActive():
-        return isActive
+    def isActive(self):
+        return self.isActive
