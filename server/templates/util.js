f298ce61e85710db5a3b1874121e4064ec201ffc
James Meredith <jam47@st-andrews.ac.uk>
2019-03-03 11:19:00 +0000

    Progress?

--- server/templates/util.js
+++ server/templates/util.js
@@ -1,47 +1,51 @@
 function sendMessage(msg) {
-    console.log("Sending: " + msg);
-    socket.send(msg);
+  console.log("Sending: " + msg);
+  socket.send(msg);
 }
 
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m connected!'});
+  socket.emit('my event', {data: 'I\'m connected!'});
 });
 socket.on('message', function(msg) {
-    console.log("Received: " + msg);
-    var obj = JSON.parse(msg);
-    if (obj.rtype == "Playlist"){
-	setSongs(obj);
-    }
     if (obj.rtype == "searchResults"){
 	setSearch(obj);
     }
+  console.log("Received: " + msg);
+  var obj = JSON.parse(msg);
+  if (obj.rtype == "songList"){
+    setSongs(obj);
+  } else if (obj.rtype == "auth") {
+    window.location = obj.data;
+  }
 });
 function upvote(e){
-    if (e.css("color") == "rgb(0, 255, 0)"){
-	e.css("color","#000000");
-	var obj = {
-	    partyid : $("#parties").find(".active").attr("id"),
-	    rtype : "sendVote",
-	    data : {
-		songid : e.parent().parent().attr("id"),
-		vote : -1.
-	    }
-	}
-	var json = JSON.stringify(obj);
-    } else {
-	e.css("color","#00ff00");
-	var obj = {
-	    partyid : $("#parties").find(".active").attr("id"),
-	    rtype : "sendVote",
+  if (e.css("color") == "rgb(0, 255, 0)"){
+    e.css("color","#000000");
+    var obj = {
+      partyid : $("#parties").find(".active").attr("id"),
+      rtype : "sendVote",
+      data : {
+        songid : e.parent().parent().attr("id"),
+        vote : -1.
+      }
+    }
+    var json = JSON.stringify(obj);
+    if (e.attr("id")){
+      sendMessage(json);
+    }
+    var json = JSON.stringify(obj);
+  } else {
+    e.css("color","#00ff00");
+    var obj = {
+      partyid : $("#parties").find(".active").attr("id"),
+      rtype : "sendVote",
 
-	    data : {
-		songid : e.parent().parent().attr("id"),
-		vote : 1 + (e.next().css("color") == "rgb(255, 0, 0)")
+      data : {
+        songid : e.parent().parent().attr("id"),
+        vote : 1 + (e.next().css("color") == "rgb(255, 0, 0)")
 
-	    }
-	}
-	var json = JSON.stringify(obj);
+      }
     }
     if (obj.partyid){
 	sendMessage(json);
@@ -49,29 +53,27 @@ function upvote(e){
     e.next().css("color","#000000");
 }
 function downvote(e){
-    if (e.css("color") == "rgb(255, 0, 0)"){
-	e.css("color","#000000");
-	var obj = {
-	    partyid : $("#parties").find(".active").attr("id"),
-	    rtype : "sendVote",
-	    data : {
-		songid : e.parent().parent().attr("id"),
-		vote : 1
-	    }
-	}
-	var json = JSON.stringify(obj);
+  if (e.css("color") == "rgb(255, 0, 0)"){
+    e.css("color","#000000");
+    var obj = {
+      partyid : $("#parties").find(".active").attr("id"),
+      rtype : "sendVote",
+      data : {
+        songid : e.parent().parent().attr("id"),
+        vote : 1
+      }
+    }
+    var json = JSON.stringify(obj);
 
-    } else {
-	e.css("color","#ff0000");
-	var obj = {
-	    partyid : $("#parties").find(".active").attr("id"),
-	    rtype : "sendVote",
-	    data : {
-		songid : e.parent().parent().attr("id"),
-		vote : -1 - (e.prev().css("color") == "rgb(0, 255, 0)")
-	    }
-	}
-	var json = JSON.stringify(obj);
+  } else {
+    e.css("color","#ff0000");
+    var obj = {
+      partyid : $("#parties").find(".active").attr("id"),
+      rtype : "sendVote",
+      data : {
+        songid : e.parent().parent().attr("id"),
+        vote : -1 - (e.prev().css("color") == "rgb(0, 255, 0)")
+      }
     }
      if (obj.partyid){
 	sendMessage(json);
@@ -79,7 +81,23 @@ function downvote(e){
     e.prev().css("color", "#000000");
 }
 function start(){
-    var unimportant = setInterval(() => {getSongs($("#parties").find(".active"))}, 2000);
+  var url_string = window.location.href
+  var url = new URL(url_string);
+  var id = url.searchParams.get("id");
+  if (id != undefined) {
+    idAttribute = $("#parties").find(".active").attr("id", id);
+    document.cookie = id;
+    authRequest = {
+      "partyid":id,
+      "rtype":"auth",
+      "data":""
+    }
+    strAuthRequest = JSON.stringify(authRequest);
+    sendMessage(strAuthRequest);
+  } else {
+    $("#parties").find(".active").attr("id", document.cookie);
+  }
+  var unimportant = setInterval(() => {getSongs($("#parties").find(".active"))}, 2000);
 }
 function getSongs(e){
     var obj = {
@@ -92,24 +110,24 @@ function getSongs(e){
     }
 }
 function setParty(e){
-    $(".active").removeClass("active");
-    e.addClass("active");
+  $(".active").removeClass("active");
+  e.addClass("active");
 }
 function setSongs(e){
-    for (i in e.data){
-	var html_out = `<li class='list-group-item' id='${i.uri}'>
-${i.name} - ${i.artists} (${i.album})
-	      <i class="material-icons float-right">
-		<span class="thup" onclick="upvote($(this))">
-		  thumb_up
-		</span>
-		<span class="thdn" onclick="downvote($(this))">
-		  thumb_down
-		</span>
-	      </i>
-	    </li>`
-	$("#tracks").append(html_out);
-    }
+  for (i in e.data){
+    var html_out = `<li class='list-group-item' id='${i.uri}'>
+    ${i.name} - ${i.artists} (${i.album})
+    <i class="material-icons float-right">
+    <span class="thup" onclick="upvote($(this))">
+    thumb_up
+    </span>
+    <span class="thdn" onclick="downvote($(this))">
+    thumb_down
+    </span>
+    </i>
+    </li>`
+    $("#tracks").append(html_out);
+  }
 }
 function search(e){
     	var obj = {
